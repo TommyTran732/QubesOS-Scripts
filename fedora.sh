28e497e65f8aa3af1481935646f4d954206ccb70
Tommy <contact@tommytran.io>
2022-02-04 10:49:08 -0500

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -8,5 +8,5 @@ sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.co
 curl --proxy http://127.0.0.1:8082 https://brave-browser-rpm-release.s3.brave.com/brave-core.asc > brave-core.asc
 sudo rpm --import brave-core.asc
 rm -rf brave-core.asc
-echo "gpgcheck=1" >> /etc/yum.repos.d/brave-browser-rpm-release.s3.brave.com_x86_64_.repo
+echo "gpgcheck=1" | sudo tee /etc/yum.repos.d/brave-browser-rpm-release.s3.brave.com_x86_64_.repo
 sudo dnf intall brave-browser -y
