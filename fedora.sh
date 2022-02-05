5c9a6c8c30d386186c7a67970a405bb9b85b2975
Tommy <contact@tommytran.io>
2022-02-05 17:52:31 -0500

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -2,7 +2,7 @@
 
 sudo dnf remove thunderbird gnome-remote-desktop gnome-calendar gnome-calculator gnome-weather gnome-contacts gnome-clock gnome-maps gnome-screenshot gnome-logs gnome-character gnome-font-viewer gnome-color-manager simple-scan keepassxc geditcheese firefox baobab yelp -y
 sudo dnf autoremove -y
-sudo dnf install arc-theme qubes-u2f seahorse -y
+sudo dnf install arc-theme qubes-u2f -y
 gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 curl --proxy http://127.0.0.1:8082 https://brave-browser-rpm-release.s3.brave.com/brave-core.asc > brave-core.asc
