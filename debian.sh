a2754e8e9c96bd2f381d591c73e5e44fbf639282
Tommy <contact@tommytran.io>
2022-02-04 20:44:50 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt purge emacs yelp keepassxc thunderbird firefox-esr gedit gnome-keyring -y
+sudo apt purge emacs emacs-gtk yelp keepassxc thunderbird firefox-esr gedit gnome-keyring -y
 sudo apt autoremove -y
