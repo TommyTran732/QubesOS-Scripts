9d17857abefc18504de1958638238dafc80fefc0
Tommy <contact@tommytran.io>
2022-02-06 15:30:24 -0500

    Update dom0.sh

--- dom0.sh
+++ dom0.sh
@@ -6,3 +6,6 @@ echo '
 gtk-theme-name=Arc-Dark
 gtk-application-prefer-dark-theme=1
 ' | sudo tee /etc/gtk-3.0/settings.ini
+
+sudo qubes-dom0-update qubes-u2f-dom0 qubes-yubikey-dom0
+qvm-service --enable work qubes-u2f-proxy
