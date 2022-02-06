826e7bf94fc2e383fdd498da414d14cb264e55b8
Tommy <contact@tommytran.io>
2022-02-06 17:08:47 -0500

    Update protonvpn.sh

--- protonvpn.sh
+++ protonvpn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
-sudo dnf install https://protonvpn.com/download/protonvpn-stable-release-1.0.1-1.noarch.rpm
-sudo dnf install protonvpn gnome-keyring -y
+sudo dnf install https://protonvpn.com/download/protonvpn-stable-release-1.0.1-1.noarch.rpm -y
+sudo dnf install protonvpn -y
