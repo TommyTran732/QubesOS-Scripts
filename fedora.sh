83b44d213bfe5a82586a8fdab917ed63e15abe6d
Tommy <contact@tommytran.io>
2022-02-07 01:08:02 -0500

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -3,13 +3,12 @@
 sudo dnf remove thunderbird totem gnome-remote-desktop gnome-calendar gnome-calculators gnome-weather gnome-contacts gnome-clocks gnome-maps gnome-screenshot gnome-logs gnome-character gnome-font-viewer gnome-color-manager simple-scan keepassxc cheese baobab yelp -y
 sudo dnf autoremove -y
 sudo dnf install arc-theme qubes-u2f -y
-sudo dnf config-manager --set-enabled rpmfusion-free
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 curl --proxy http://127.0.0.1:8082 https://brave-browser-rpm-release.s3.brave.com/brave-core.asc > brave-core.asc
 sudo rpm --import brave-core.asc
 rm -rf brave-core.asc
 echo "gpgcheck=1" | sudo tee /etc/yum.repos.d/brave-browser-rpm-release.s3.brave.com_x86_64_.repo
-sudo dnf intall brave-browser vlc -y
+sudo dnf intall brave-browser -y
 
 git config --global http.proxy http://127.0.0.1:8082
 git clone https://github.com/horst3180/arc-icon-theme
