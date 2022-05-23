dbeba2fbe66e838693231855481bbdc6c383f607
Tommy <contact@tommytran.io>
2022-05-23 05:17:18 -0400

    Update usb.sh

--- usb.sh
+++ usb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo dnf install gnome-disk-utility ykpers yubikey-personalization-gui yubioath nitrokey-app 
+sudo dnf install gnome-disk-utility yubikey-manager-qt yubioath nitrokey-app 
