c6190b8aadf84bcfbf28d7d351a676ba78b5adc4
Tommy <contact@tommytran.io>
2022-05-23 04:55:31 -0400

    Update protonvpn.sh

--- protonvpn.sh
+++ protonvpn.sh
@@ -1,5 +1,13 @@
 #!/bin/bash 
 
-#Base this on fedora-X-minimal-sys-net
+
+sudo dnf install -y qubes-core-agent-networking qubes-core-agent-network-manager NetworkManager-open network-manager-applet notification-daemon gnome-keyring polkit arc-theme
+
+sudo mkdir /etc/gtk-3.0
+echo '[Settings]
+gtk-theme-name=Arc-Dark
+gtk-application-prefer-dark-theme=1
+' | sudo tee /etc/gtk-3.0/settings.ini
+
 sudo dnf install https://protonvpn.com/download/protonvpn-stable-release-1.0.1-1.noarch.rpm -y
-sudo dnf install protonvpn NetworkManager-openvpn -y
+sudo dnf install protonvpn -y
