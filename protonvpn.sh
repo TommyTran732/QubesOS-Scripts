3f76b5e2ec90695289ff83b9d4364513d1ea3b15
Tommy <contact@tommytran.io>
2022-05-23 04:48:31 -0400

    Update protonvpn.sh

--- protonvpn.sh
+++ protonvpn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
+#Base this on fedora-X-minimal-sys-net
 sudo dnf install https://protonvpn.com/download/protonvpn-stable-release-1.0.1-1.noarch.rpm -y
-sudo dnf install protonvpn -y
+sudo dnf install protonvpn NetworkManager-openvpn -y
