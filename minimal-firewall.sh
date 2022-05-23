63ba0c472b982e9be2fcd891073b70920a13f174
Tommy <contact@tommytran.io>
2022-05-23 04:19:09 -0400

    Update minimal-firewall.sh

--- minimal-firewall.sh
+++ minimal-firewall.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-sudo dnf install -y qubes-core-agent-networking iproute qubes-core-agent-dom0-updates arc-theme
-
-sudo mkdir /etc/gtk-3.0
-echo '[Settings]
-gtk-theme-name=Arc-Dark
-gtk-application-prefer-dark-theme=1
-' | sudo tee /etc/gtk-3.0/settings.ini
+sudo dnf install -y qubes-core-agent-networking iproute qubes-core-agent-dom0-updates
