cd7a2d911fe1fad34d16199452cd07da37099cd5
Tommy <contact@tommytran.io>
2022-05-23 06:30:51 -0400

    Update whonix-hardening.sh

--- whonix-hardening.sh
+++ whonix-hardening.sh
@@ -7,3 +7,6 @@ systemctl enable --now permission-hardening
 apt update
 apt full-upgrade -y
 sudo apt install --no-install-recommends lkrg-dkms linux-headers-amd64
+
+#Enable hardened malloc
+echo "/usr/lib/libhardened_malloc.so/libhardened_malloc.so" >> /etc/ld.so.preload
