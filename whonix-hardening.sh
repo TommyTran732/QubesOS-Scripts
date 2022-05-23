be6bce083a25c9e73a213659c1d57630c492440f
Tommy <contact@tommytran.io>
2022-05-22 20:09:39 -0400

    Update whonix-hardening.sh

--- whonix-hardening.sh
+++ whonix-hardening.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+#Enabling SUID Disabler and Permission Hardener
 systemctl enable --now permission-hardening
+
+#Install LKRG
+apt update
+apt full-upgrade -y
+sudo apt install --no-install-recommends lkrg-dkms linux-headers-amd64
