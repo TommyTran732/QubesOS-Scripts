862445017ed41a191bd22d17c44b95898830dbb5
Tommy <contact@tommytran.io>
2022-05-24 09:37:54 -0400

    Update minimal-vault-gpg.sh

--- minimal-vault-gpg.sh
+++ minimal-vault-gpg.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo dnf install -y qubes-gpg-split
+sudo dnf install -y qubes-gpg-split arc-theme
+
+sudo mkdir /etc/gtk-3.0
+echo '[Settings]
+gtk-theme-name=Arc-Dark
+gtk-application-prefer-dark-theme=1
+' | sudo tee /etc/gtk-3.0/settings.ini
