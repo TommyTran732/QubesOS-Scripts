47b46127d0e3ee38c2180669250f06e56dd6ed2a
Tommy <contact@tommytran.io>
2022-05-24 07:46:36 -0400

    Update whonix-hardening.sh

--- whonix-hardening.sh
+++ whonix-hardening.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #Enabling SUID Disabler and Permission Hardener
-systemctl enable --now permission-hardening
+sudo systemctl enable --now permission-hardening
 
 #Install LKRG
-apt update
-apt full-upgrade -y
+sudo apt update
+sudo apt full-upgrade -y
 sudo apt install --no-install-recommends lkrg-dkms linux-headers-amd64
 
 #Enable hardened malloc
-echo "/usr/lib/libhardened_malloc.so/libhardened_malloc.so" >> /etc/ld.so.preload
+echo "/usr/lib/libhardened_malloc.so/libhardened_malloc.so" | sudo tee /etc/ld.so.preload
 
 #Reduce kernel information leaks
 #Will break a lot of applications. The apps I use on Whonix work fine with it so I am enabling it.
-systemctl enable --now hide-hardware-info.service
+sudo systemctl enable --now hide-hardware-info.service
