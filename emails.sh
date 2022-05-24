3cf5ddad3c160d9853c265353e9091d626963049
Tommy <contact@tommytran.io>
2022-05-24 08:24:09 -0400

    Update emails.sh

--- emails.sh
+++ emails.sh
@@ -4,4 +4,4 @@ sudo dnf install thunderbird -y
 
 #Do this in the AppVM after you have set it up
 #Obviously replace sys-usb with the actual GPG backend that you are using https://www.qubes-os.org/doc/split-gpg/
-echo "sys-usb" | sudo tee /rw/config/gpg-split-domain
+#echo "vault-gpg" | sudo tee /rw/config/gpg-split-domain
