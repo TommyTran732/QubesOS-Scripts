c7312032b857055f46659c7a5bc2559a79545cc3
Tommy <contact@tommytran.io>
2022-05-24 11:02:48 -0400

    Update minimal-protonvpn.sh

--- minimal-protonvpn.sh
+++ minimal-protonvpn.sh
@@ -1,13 +1,10 @@
 #!/bin/bash 
 
-
-sudo dnf install -y qubes-core-agent-networking qubes-core-agent-network-manager NetworkManager-open network-manager-applet notification-daemon gnome-keyring polkit arc-theme
-
-sudo mkdir /etc/gtk-3.0
-echo '[Settings]
-gtk-theme-name=Arc-Dark
-gtk-application-prefer-dark-theme=1
-' | sudo tee /etc/gtk-3.0/settings.ini
+sudo cat > /etc/dconf/db/local.d/custom <<- 'EOF'
+[org/gnome/desktop/interface]
+gtk-theme='Arc-Dark'
+icon-theme='Arc'
+EOF
 
 sudo dnf install https://protonvpn.com/download/protonvpn-stable-release-1.0.1-1.noarch.rpm -y
 sudo dnf install protonvpn -y
