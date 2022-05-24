46035b897288ff0803316f35a79fc7da149525c5
Tommy <contact@tommytran.io>
2022-05-24 11:03:15 -0400

    Update protonvpn.sh

--- protonvpn.sh
+++ protonvpn.sh
@@ -3,7 +3,6 @@
 sudo cat > /etc/dconf/db/local.d/custom <<- 'EOF'
 [org/gnome/desktop/interface]
 gtk-theme='Arc-Dark'
-icon-theme='Arc'
 EOF
 
 sudo dnf install https://protonvpn.com/download/protonvpn-stable-release-1.0.1-1.noarch.rpm -y
