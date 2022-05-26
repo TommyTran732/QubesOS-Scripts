f5c8a68cc348db5d452de1cc347c448789803de5
Tommy <contact@tommytran.io>
2022-05-26 00:44:12 -0400

    Update debian.sh

--- debian/debian.sh
+++ debian/debian.sh
@@ -3,6 +3,9 @@
 sudo apt purge -y thunderbird emacs emacs-gtk emacs-bin-common emacs-common firefox* keepassxc cups* vim* system-config-printer* xsettingsd xterm* yelp*
 sudo apt autoremove -y
 sudo apt autoclean
+
+sudo curl --proxy http://127.0.0.1:8082 -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt install -y qt5ct qt5-style-plugins arc-theme
 
 git config --global http.proxy http://127.0.0.1:8082
