baa5ce5419cd4c90b062ea5fb435b013e9e60ac5
Tommy <contact@tommytran.io>
2022-05-25 22:16:33 -0400

    Update debian.sh

--- debian/debian.sh
+++ debian/debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt purge -y thunderbird emacs* firefox* keepassxc cups* vim* system-config-printer*
+sudo apt purge -y thunderbird emacs firefox* keepassxc cups* vim* system-config-printer*
 sudo apt autoremove
