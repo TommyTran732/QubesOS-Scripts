6da3655793a7acbf59476428f8bca7bc3c78afa5
Tommy <contact@tommytran.io>
2022-05-25 23:35:16 -0400

    Update debian.sh

--- debian/debian.sh
+++ debian/debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt purge -y thunderbird emacs emacs-gtk emacs-bin-common emacs-common firefox* keepassxc cups* vim* system-config-printer* xsettingsd xterm*
+sudo apt purge -y thunderbird emacs emacs-gtk emacs-bin-common emacs-common firefox* keepassxc cups* vim* system-config-printer* xsettingsd xterm* yelp*
 sudo apt autoremove -y
 sudo apt autoclean
 sudo apt install -y qt5ct qt5-qtstyleplugins arc-theme
