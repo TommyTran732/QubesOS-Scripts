cf695350fc73bec6eb2401451124c0d98266ae31
Tommy <contact@tommytran.io>
2022-05-25 23:22:56 -0400

    Update debian.sh

--- debian/debian.sh
+++ debian/debian.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-sudo apt purge -y thunderbird emacs firefox* keepassxc cups* vim* system-config-printer*
+sudo apt purge -y thunderbird emacs firefox* keepassxc cups* vim* system-config-printer* xsettingsd
 sudo apt autoremove
+sudo apt install qt5ct qt5-qtstyleplugins arc-theme
 
-sudo apt install arc-theme
+sudo mkdir /etc/gtk-3.0
+echo '[Settings]
+gtk-theme-name=Arc-Dark
+gtk-application-prefer-dark-theme=1
+' | sudo tee /etc/gtk-3.0/settings.ini
+
+sudo mkdir /etc/gtk-4.0
+echo '[Settings]
+gtk-theme-name=Arc-Dark
+gtk-application-prefer-dark-theme=1
+' | sudo tee /etc/gtk-4.0/settings.ini
+
+echo "export export QT_QPA_PLATFORMTHEME=gtk2" | sudo tee /etc/environment
