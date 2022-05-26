3441f38b806132269457ffa77239966ff6444297
Tommy <contact@tommytran.io>
2022-05-26 00:35:09 -0400

    Update debian.sh

--- debian/debian.sh
+++ debian/debian.sh
@@ -5,6 +5,13 @@ sudo apt autoremove -y
 sudo apt autoclean
 sudo apt install -y qt5ct qt5-style-plugins arc-theme
 
+git config --global http.proxy http://127.0.0.1:8082
+git clone https://github.com/horst3180/arc-icon-theme
+mv arc-icon-theme/Arc /usr/share/icons
+rm -rf arc-icon-theme
+
+echo "export export QT_QPA_PLATFORMTHEME=gtk2" | sudo tee /etc/environment
+
 sudo mkdir /etc/gtk-3.0
 echo '[Settings]
 gtk-theme-name=Arc-Dark
@@ -17,4 +24,8 @@ gtk-theme-name=Arc-Dark
 gtk-application-prefer-dark-theme=1
 ' | sudo tee /etc/gtk-4.0/settings.ini
 
-echo "export export QT_QPA_PLATFORMTHEME=gtk2" | sudo tee /etc/environment
+sudo cat > /etc/dconf/db/local.d/custom <<- 'EOF'
+[org/gnome/desktop/interface]
+gtk-theme='Arc-Dark'
+icon-theme='Arc'
+EOF
