3504a17724e21bd1de3ea6efa504906f25407c20
Tommy <contact@tommytran.io>
2022-05-27 00:15:30 -0400

    Update element.sh

--- kicksecure/element.sh
+++ kicksecure/element.sh
@@ -4,3 +4,6 @@ sudo curl --proxy http://127.0.0.1:8082/ https://packages.element.io/debian/elem
 echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
 sudo apt update
 sudo apt install element-desktop
+
+#Adding a DNS entry for my Matrix server here so I can add a Firewall rule locking the AppVM to only being able to connect to my server.
+echo "5.226.143.168 matrix.arcticfoxes.net" >> /etc/hosts
