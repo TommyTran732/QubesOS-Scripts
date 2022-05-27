e1c04989dae7bd92cadd73e7c701c9f31990801a
Tommy <contact@tommytran.io>
2022-05-27 00:22:31 -0400

    Update nextcloud.sh

--- kicksecure/nextcloud.sh
+++ kicksecure/nextcloud.sh
@@ -3,4 +3,4 @@
 sudo apt install --no-install-recommends nextcloud-client
 
 #Adding a DNS entry for my Nextcloud server here so I can add a Firewall rule locking the AppVM to only being able to connect to my server.
-echo "5.226.143.92 cloud.tommytran.io" >> /etc/hosts
+echo "5.226.143.92 cloud.tommytran.io" | sudo tee -a /etc/hosts
