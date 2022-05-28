f4ed8cd603a295c2840385ca43e12cee05101dd0
Tommy <contact@tommytran.io>
2022-05-28 08:14:18 -0400

    Minor fixes

---
+++ kicksecure/brave.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo dnf install -y https://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
+sudo dnf install -y gnome-calculator onlyoffice-desktopeditors lyx gimp
