9ca9c32377b72f71d22758695ba8ade46770abce
Tommy <contact@tommytran.io>
2022-06-01 05:51:38 -0400

    Update nextcloud.sh

--- fedora/nextcloud.sh
+++ fedora/nextcloud.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 sudo dnf install -y nextcloud-client
-
-#Adding a DNS entry for my Nextcloud server here so I can add a Firewall rule locking the AppVM to only being able to connect to my server.
-echo "5.226.143.92 cloud.tommytran.io" | sudo tee -a /etc/hosts
