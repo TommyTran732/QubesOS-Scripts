3c3483f2d349ec4d384aa19e097a8c59ae3db6f5
Tommy <contact@tommytran.io>
2022-06-14 21:18:05 -0400

    Create kicksecure.sh

---
+++ kicksecure-minimal/kicksecure.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Adding KickSecure's signing key
+curl --proxy http://127.0.0.1:8082/ --tlsv1.3 --proto =https --max-time 180 --output ~/derivative.asc https://www.kicksecure.com/derivative.asc
+sudo cp ~/derivative.asc /usr/share/keyrings/derivative.asc
+echo "deb [signed-by=/usr/share/keyrings/derivative.asc] https://deb.kicksecure.com bullseye main contrib non-free" | sudo tee /etc/apt/sources.list.d/derivative.list
+
+#Distribution morphing
+sudo apt install --no-install-recommends kicksecure-qubes-cli -y
+sudo apt autoremove -y
+sudo mv /etc/apt/sources.list ~/
+sudo touch /etc/apt/sources.list
+
+#Enabling SUID Disabler and Permission Hardener
+sudo systemctl enable --now permission-hardening
+
+#Install LKRG
+sudo apt install --no-install-recommends linux-headers-amd64 lkrg-dkms -y
+
+#Enable hardened malloc
+echo "/usr/lib/libhardened_malloc.so/libhardened_malloc.so" | sudo tee /etc/ld.so.preload
+
+#Reduce kernel information leaks
+#Will break a lot of applications. The apps I use on KickSecure work fine with it so I am enabling it.
+sudo systemctl enable --now hide-hardware-info.service
