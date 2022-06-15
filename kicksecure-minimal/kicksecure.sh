8b347ebb0e898c09325a6af3868ddd7aea57fe8b
Tommy <contact@tommytran.io>
2022-06-14 21:45:07 -0400

    Update kicksecure.sh

--- kicksecure-minimal/kicksecure.sh
+++ kicksecure-minimal/kicksecure.sh
@@ -6,9 +6,9 @@ sudo apt install curl -y
 curl --proxy http://127.0.0.1:8082/ --tlsv1.3 --proto =https --max-time 180 --output ~/derivative.asc https://www.kicksecure.com/derivative.asc
 sudo cp ~/derivative.asc /usr/share/keyrings/derivative.asc
 echo "deb [signed-by=/usr/share/keyrings/derivative.asc] https://deb.kicksecure.com bullseye main contrib non-free" | sudo tee /etc/apt/sources.list.d/derivative.list
+sudo apt update
 
 #Distribution morphing
-sudo apt update
 sudo apt install --no-install-recommends kicksecure-qubes-cli -y
 sudo apt autoremove -y
 sudo mv /etc/apt/sources.list ~/
