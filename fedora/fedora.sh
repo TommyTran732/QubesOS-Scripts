c694e643e105596ef7355c571135bcd960bb637d
Tommy <contact@tommytran.io>
2022-07-01 08:51:06 -0400

    Update fedora.sh

--- fedora/fedora.sh
+++ fedora/fedora.sh
@@ -2,7 +2,7 @@
 
 sudo dnf remove firefox thunderbird totem gnome-remote-desktop gnome-calendar gnome-disk-utility gnome-calculator gnome-connections gnome-weather gnome-contacts gnome-clocks gnome-maps gnome-screenshot gnome-logs gnome-characters gnome-font-viewer gnome-color-manager simple-scan keepassxc cheese baobab yelp evince* httpd mozilla* cups rygel -y
 sudo dnf autoremove -y
-sudo dnf install qubes-u2f qubes-gpg-split -y
+sudo dnf install qubes-u2f qubes-gpg-split arc-theme qt5ct qt5-qtstyleplugins -y
 echo "countme=False" | sudo tee -a /etc/dnf/dnf.conf
 
 git config --global http.proxy http://127.0.0.1:8082
@@ -10,8 +10,12 @@ git clone https://github.com/horst3180/arc-icon-theme
 sudo mv arc-icon-theme/Arc /usr/share/icons
 rm -rf arc-icon-theme
 
+echo "export QT_QPA_PLATFORMTHEME=gtk2" | sudo tee /etc/environment
+echo "export GTK_THEME=Arc-Dark" | sudo tee -a /etc/environment
+
 echo "[org/gnome/desktop/interface]
 color-scheme='prefer-dark'
+gtk-theme='Arc-Dark'
 icon-theme='Arc'" | sudo tee /etc/dconf/db/local.d/custom
 
 sudo dconf update
