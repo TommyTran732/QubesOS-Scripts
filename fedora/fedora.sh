cb86ac6e79a23cfb17a6d257b2ff51814f6cbab2
Tommy <contact@tommytran.io>
2022-07-01 08:00:40 -0400

    Update fedora.sh

--- fedora/fedora.sh
+++ fedora/fedora.sh
@@ -3,7 +3,7 @@
 sudo dnf remove firefox thunderbird totem gnome-remote-desktop gnome-calendar gnome-disk-utility gnome-calculator gnome-connections gnome-weather gnome-contacts gnome-clocks gnome-maps gnome-screenshot gnome-logs gnome-characters gnome-font-viewer gnome-color-manager simple-scan keepassxc cheese baobab yelp evince* httpd mozilla* cups rygel -y
 sudo dnf autoremove -y
 sudo dnf install qubes-u2f qubes-gpg-split arc-theme qt5ct qt5-qtstyleplugins -y
-echo "countme=false" | sudo tee -a /etc/dnf/dnf.conf
+echo "countme=False" | sudo tee -a /etc/dnf/dnf.conf
 
 git config --global http.proxy http://127.0.0.1:8082
 git clone https://github.com/horst3180/arc-icon-theme
