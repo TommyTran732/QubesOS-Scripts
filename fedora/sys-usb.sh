d58edcc5df5c9c113c453dd225a958d7ed4b9e97
Tommy <contact@tommytran.io>
2022-07-01 10:47:58 -0400

    Update sys-usb.sh

--- fedora/sys-usb.sh
+++ fedora/sys-usb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo dnf install -y qubes-usb-proxy qubes-input-proxy-sender qubes-u2f gnome-disk-utility yubikey-manager-qt yubioath nitrokey-app
+sudo dnf install -y gnome-disk-utility yubikey-manager-qt yubioath nitrokey-app
