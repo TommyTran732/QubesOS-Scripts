fdc1f091674ef57793c8c4173a635cbe0a10aaf4
Tommy <contact@tommytran.io>
2022-07-01 10:25:26 -0400

    Update protonvpn.sh

--- fedora/protonvpn.sh
+++ fedora/protonvpn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl --proxy http://127.0.0.1:8082/ -O https://protonvpn.com/download/protonvpn-stable-release-1.0.1-1.noarch.rpm
+sudo dnf install https://protonvpn.com/download/protonvpn-stable-release-1.0.1-1.noarch.rpm -y
 sudo dnf install protonvpn -y
