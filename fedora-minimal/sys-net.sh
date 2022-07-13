655c01ccf5f44c105d28727e5402ff2448a0ce0b
Tommy <contact@tommytran.io>
2022-07-13 09:03:49 -0400

    Update sys-net.sh

--- fedora-minimal/sys-net.sh
+++ fedora-minimal/sys-net.sh
@@ -3,7 +3,8 @@
 sudo dnf install -y qubes-core-agent-networking qubes-core-agent-network-manager NetworkManager-wifi network-manager-applet notification-daemon gnome-keyring @hardware-support chrony arc-theme
 
 sudo systemctl disable --now systemd-timesyncd
-sudo curl --proxy http://127.0.0.1:8082 https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chrony.conf -o /etc/chrony/chrony.conf
+sudo rm -rf /etc/chrony.conf
+sudo curl --proxy http://127.0.0.1:8082 https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chrony.conf -o /etc/chrony.conf
 sudo systemctl enable --now chronyd
 
 sudo mkdir -p /etc/gtk-3.0
