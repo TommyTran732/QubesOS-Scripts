03b7dfa0344a4c9b2db51935ce41aa3c1070d940
Tommy <contact@tommytran.io>
2022-07-26 05:57:13 -0400

    Update lokinet.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- kicksecure/lokinet.sh
+++ kicksecure/lokinet.sh
@@ -8,5 +8,5 @@ sudo curl --proxy http://127.0.0.1:8082 -fsSLo /usr/share/keyrings/brave-browser
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install --no-install-recommends brave-browser lokinet-gui
-sed -i 's/#exit-node=/exit-node=exit.loki/' /var/lib/lokinet/lokinet.ini
-systemctl enable --now lokinet
+sudo sed -i 's/#exit-node=/exit-node=exit.loki/' /var/lib/lokinet/lokinet.ini
+sudo systemctl enable --now lokinet
