7baa3f282e2ee2fac59dca6295fb15469f928349
Tommy <contact@tommytran.io>
2022-08-02 01:51:10 -0400

    Update sys-usb.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- fedora/sys-usb.sh
+++ fedora/sys-usb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo dnf install -y gnome-disk-utility yubikey-manager-qt yubioath nitrokey-app
+sudo dnf install -y gnome-disk-utility yubikey-manager-qt yubioath nitrokey-app android-tools
