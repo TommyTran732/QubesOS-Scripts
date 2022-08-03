23ab8b9461ca19c67a8b712a8537e8b1c0e49d4d
Tommy <contact@tommytran.io>
2022-08-03 08:21:08 -0400

    Update emails.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- fedora/emails.sh
+++ fedora/emails.sh
@@ -2,9 +2,6 @@
 
 sudo dnf install thunderbird -y
 
-#Remove if you don't use ProtonMail
-sudo dnf install https://protonmail.com/download/bridge/protonmail-bridge-2.1.3-1.x86_64.rpm -y
-
 #Do this in the AppVM after you have set it up
 #Obviously replace vault-gpg with the actual GPG backend that you are using https://www.qubes-os.org/doc/split-gpg/
 #echo "vault" | sudo tee /rw/config/gpg-split-domain
