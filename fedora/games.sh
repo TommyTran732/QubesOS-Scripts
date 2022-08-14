0d70cbfa7265a5a3b4320a4c06fc33d92e25cb93
tommytran732 <contact@tommytran.io>
2022-08-14 18:57:12 -0400

    Minor fixes

--- fedora/games.sh
+++ fedora/games.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "color-scheme='prefer-dark'" | sudo tee -a /etc/dconf/local.d/custom
+echo "color-scheme='prefer-dark'" | sudo tee -a /etc/dconf/db/local.d/custom
 sudo dconf update
 
 sudo dnf install -y gnome-chess gnome-2048 gnome-mines
--- kali.sh
+++ kali.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+#Unmark hold
+sudo apt-mark unhold libqubes-rpc-filecopy2 libqubesdb pulseaudio-qubes python3-qubesdb python3-qubesimgconverter qubes-core-agent-dom0-updates qubes-core-agent-nautilus qubes-core-agent-network-manager qubes-core-agent-passwordless-root qubes-core-agent qubes-core-qrexec qubes-gpg-split qubes-gui-agent qubes-img-converter qubes-input-proxy-sender qubes-kernel-vm-support qubes-mgmt-salt-vm-connector qubes-pdf-converter qubes-u2f qubes-usb-proxy qubes-utils qubes-vm-dependencies qubes-vm-recommended qubesdb-vm qubesdb xserver-xorg-input-qubes xserver-xorg-qubes-common
+
 #Debloat
 sudo apt purge firefox* chromium* thunderbird* emacs emacs-gtk emacs-bin-common emacs-common xsettingsd yelp* -y
 sudo apt autoremove -y
 
 #Install packages
 sudo apt install --no-install-recommends qubes-gpg-split qubes-u2f eog qt5ct qt5-style-plugins arc-theme goldeneye -y
-
-#Unmark hold
-sudo apt-mark unhold libqubes-rpc-filecopy2 libqubesdb pulseaudio-qubes python3-qubesdb python3-qubesimgconverter qubes-core-agent-dom0-updates qubes-core-agent-nautilus qubes-core-agent-network-manager qubes-core-agent-passwordless-root qubes-core-agent qubes-core-qrexec qubes-gpg-split qubes-gui-agent qubes-img-converter qubes-input-proxy-sender qubes-kernel-vm-support qubes-mgmt-salt-vm-connector qubes-pdf-converter qubes-u2f qubes-usb-proxy qubes-utils qubes-vm-dependencies qubes-vm-recommended qubesdb-vm qubesdb xserver-xorg-input-qubes xserver-xorg-qubes-common
 
 #Theming
 git config --global http.proxy http://127.0.0.1:8082
