906d0a3b545560403561323d8bbe94a67b2c0252
Tommy <contact@tommytran.io>
2022-08-15 01:26:53 -0400

    Update mullvad.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- fedora/mullvad.sh
+++ fedora/mullvad.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Needs to be installed in a standalone VM for now.
 
 sudo dnf install -y https://mullvad.net/media/app/MullvadVPN-2022.3_x86_64.rpm
 
@@ -11,7 +12,8 @@ sudo rm -rf /usr/share/icons/Arc
 
 echo "/usr/lib/qubes/qubes-setup-dnat-to-ns" | sudo tee -a /rw/config/rc.local
 
-sudo mkdir -p /etc/qubes-bind-dirs.d
-sudo tee /etc/qubes-bind-dirs.d50_user.conf << EOF > /dev/null
-binds+=( '/etc/mullvad-vpn' )
-EOF
+#I am still missing something here to make it work in a TemplateVM->AppVM setup... not sure what right now.
+#sudo mkdir -p /etc/qubes-bind-dirs.d
+#sudo tee /etc/qubes-bind-dirs.d50_user.conf << EOF > /dev/null
+#binds+=( '/etc/mullvad-vpn' )
+#EOF
