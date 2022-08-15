8bb40b0e5eee4177c05d3b515bed92ae4626fc18
Tommy <contact@tommytran.io>
2022-08-15 00:07:27 -0400

    Update mullvad.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- fedora/mullvad.sh
+++ fedora/mullvad.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#This needs to be in a standalone VM
 
-sudo dnf install -y https://mullvad.net/media/app/MullvadVPN-2022.2_x86_64.rpm
+sudo dnf install -y https://mullvad.net/media/app/MullvadVPN-2022.3_x86_64.rpm
 
 echo "[org/gnome/desktop/interface]
 gtk-theme='Arc-Dark'" | sudo tee /etc/dconf/db/local.d/custom
@@ -11,3 +10,8 @@ sudo dconf update
 sudo rm -rf /usr/share/icons/Arc
 
 echo "/usr/lib/qubes/qubes-setup-dnat-to-ns" | sudo tee -a /rw/config/rc.local
+
+sudo mkdir -p /etc/qubes-bind-dirs.d
+sudo tee /etc/qubes-bind-dirs.d50_user.conf << EOF > /dev/null
+binds+=( '/etc/mullvad-vpn' )
+EOF
