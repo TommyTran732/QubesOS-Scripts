8d147e0fb3b6f7c79be811b6be33d94c60e7df27
Tommy <contact@tommytran.io>
2022-09-01 02:49:52 -0400

    Update protonvpn.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- fedora/protonvpn.sh
+++ fedora/protonvpn.sh
@@ -4,7 +4,11 @@ sudo dnf install https://protonvpn.com/download/protonvpn-stable-release-1.0.1-1
 sudo dnf install protonvpn -y
 
 echo "[org/gnome/desktop/interface]
-gtk-theme='Arc-Dark'" | sudo tee /etc/dconf/db/local.d/custom
+gtk-theme='Arc-Dark'
+
+[org/gnome/desktop/media-handling]
+automount=false
+automount-open=false" | sudo tee /etc/dconf/db/local.d/custom
 
 sudo dconf update
 
