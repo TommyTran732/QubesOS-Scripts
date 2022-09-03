6bd8ad07c21de8748f9046ffc40c5f980bf5c97e
Tommy <contact@tommytran.io>
2022-09-03 06:04:46 -0400

    Mullvad fully working
    Signed-off-by: Tommy <contact@tommytran.io>

--- fedora/mullvad.sh
+++ fedora/mullvad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#Needs to be installed in a standalone VM for now.
 
 sudo dnf install -y https://mullvad.net/media/app/MullvadVPN-2022.4_x86_64.rpm
+sudo systemctl enable mullvad-daemon
 
 echo "[org/gnome/desktop/interface]
 gtk-theme='Arc-Dark'
@@ -14,10 +14,11 @@ sudo dconf update
 
 sudo rm -rf /usr/share/icons/Arc
 
-echo "/usr/lib/qubes/qubes-setup-dnat-to-ns" | sudo tee -a /rw/config/rc.local
+sudo mkdir -p /etc/qubes-bind-dirs.d
+sudo tee /etc/qubes-bind-dirs.d/50_user.conf << EOF > /dev/null
+binds+=( '/etc/mullvad-vpn' )
+EOF
 
-#I am still missing something here to make it work in a TemplateVM->AppVM setup... not sure what right now.
-#sudo mkdir -p /etc/qubes-bind-dirs.d
-#sudo tee /etc/qubes-bind-dirs.d50_user.conf << EOF > /dev/null
-#binds+=( '/etc/mullvad-vpn' )
-#EOF
+#Run these in the AppVM:
+#echo "sleep 10 # Waiting a bit that Mullvad succeeds to establish connection
+#/usr/lib/qubes/qubes-setup-dnat-to-ns" | sudo tee -a /rw/config/rc.local
