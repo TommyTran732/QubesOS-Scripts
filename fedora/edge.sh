98c033c3eb5a86cd4d2af0163a67989cfc5b4609
Tommy <contact@tommytran.io>
2022-10-13 18:04:24 -0400

    Update edge.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- fedora/edge.sh
+++ fedora/edge.sh
@@ -2,5 +2,6 @@
 
 curl --proxy http://127.0.0.1:8082 -O https://packages.microsoft.com/keys/microsoft.asc
 sudo rpm --import microsoft.asc
+rm microsoft.asc
 sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
 sudo dnf install -y microsoft-edge-stable
