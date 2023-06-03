9edd838740cc9b81f5b094f59f67381e9adb3252
Tommy <contact@tommytran.io>
2023-06-03 02:50:28 -0700

    Remove hugo
    Signed-off-by: Tommy <contact@tommytran.io>

--- fedora/development.sh
+++ fedora/development.sh
@@ -6,7 +6,7 @@ sudo rpm --import shiftkey.asc
 sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://mirror.mwt.me/ghd/rpm\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://mirror.mwt.me/ghd/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo'
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-compose-plugin github-desktop code hugo
+sudo dnf install docker-ce docker-compose-plugin github-desktop code
 sudo systemctl enable --now docker
 sudo usermod -aG docker $USER
 newgrp docker
