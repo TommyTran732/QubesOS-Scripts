0065bf3d3a2bc38d2d2c15846f52fc618315671f
Tommy <contact@tommytran.io>
2023-06-22 01:37:07 -0700

    Remove arch pin
    Signed-off-by: Tommy <contact@tommytran.io>

--- kicksecure/lokinet.sh
+++ kicksecure/lokinet.sh
@@ -19,7 +19,7 @@ gtk-application-prefer-dark-theme=1" | sudo tee /etc/gtk-4.0/settings.ini
 sudo curl --proxy http://127.0.0.1:8082 -so /etc/apt/trusted.gpg.d/oxen.gpg https://deb.oxen.io/pub.gpg
 echo "deb https://deb.oxen.io $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/oxen.list
 sudo curl --proxy http://127.0.0.1:8082 -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install --no-install-recommends brave-browser lokinet-gui resolvconf
 sudo sed -i 's/#exit-node=/exit-node=exit.loki/' /var/lib/lokinet/lokinet.ini
