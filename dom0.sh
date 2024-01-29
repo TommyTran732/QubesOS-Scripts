e780493bd258deada89e10a6e66d17b27f7649db
Tommy <contact@tommytran.io>
2024-01-28 17:41:00 -0700

    Remove theatre
    Signed-off-by: Tommy <contact@tommytran.io>

--- dom0.sh
+++ dom0.sh
@@ -24,7 +24,7 @@ echo "[Settings]
 gtk-theme-name=Arc-Dark
 gtk-application-prefer-dark-theme=1" | sudo tee /etc/gtk-3.0/settings.ini
 
-sudo qubes-dom0-update qubes-u2f-dom0 qubes-yubikey-dom0 qt5ct qt5-qtstyleplugins
+sudo qubes-dom0-update qubes-u2f-dom0 qt5ct qt5-qtstyleplugins
 qvm-service --enable personal qubes-u2f-proxy
 qvm-service --enable work qubes-u2f-proxy
 
