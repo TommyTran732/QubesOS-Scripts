7ca046d5091627f841a60de6e5137b5655a2130b
Tommy <contact@tommytran.io>
2024-01-28 17:47:01 -0700

    Remove things configurable via the UI
    Signed-off-by: Tommy <contact@tommytran.io>

--- dom0.sh
+++ dom0.sh
@@ -31,19 +31,5 @@ gtk-theme-name=Arc-Dark
 gtk-application-prefer-dark-theme=1" | sudo tee /etc/gtk-3.0/settings.ini
 
 sudo qubes-dom0-update qubes-u2f-dom0 qt5ct qt5-qtstyleplugins
-qvm-service --enable personal qubes-u2f-proxy
-qvm-service --enable work qubes-u2f-proxy
 
 echo "QT_QPA_PLATFORMTHEME=qt5ct" | sudo tee /etc/environment
-
-#O bviously replace vault with the actual GPG backend that you are using https://www.qubes-os.org/doc/split-gpg/
-echo "emails  vault allow" | sudo tee /etc/qubes-rpc/policy/qubes.Gpg
-echo "@anyvm  @anyvm  ask,default_target=vault" | sudo tee -a /etc/qubes-rpc/policy/qubes.Gpg
-
-# Same thing, but for split SSH. No default allow here because here though because there will not be a timeout or anything like that.
-echo "@anyvm  @anyvm ask,default_target=vault" | sudo tee /etc/qubes-rpc/policy/qubes.SshAgent
-
-# Enabling VMAuth - if you want to get the prompt you will still need to configure the guest VMs tho
-echo "/usr/bin/echo 1" | sudo tee /etc/qubes-rpc/qubes.VMAuth
-sudo chmod u+x /etc/qubes-rpc/qubes.VMAuth
-echo "@anyvm dom0 ask,default_target=dom0" | sudo tee /etc/qubes-rpc/policy/qubes.VMAuth
