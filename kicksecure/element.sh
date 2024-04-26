c4034e5976ce50581194bfc5d55ec90ee190bd28
Tommy <contact@tommytran.io>
2024-04-26 15:58:59 -0700

    Remove /etc/hosts hardcode
    Signed-off-by: Tommy <contact@tommytran.io>

--- kicksecure/element.sh
+++ kicksecure/element.sh
@@ -18,6 +18,3 @@ curl --proxy http://127.0.0.1:8082/ https://packages.element.io/debian/element-i
 echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
 sudo apt update
 sudo apt install --no-install-recommends element-desktop -y
-
-# Adding a DNS entry for my Matrix server here so I can add a Firewall rule locking the AppVM to only being able to connect to my server.
-echo "5.226.143.168 matrix.arcticfoxes.net" | sudo tee -a /etc/hosts
