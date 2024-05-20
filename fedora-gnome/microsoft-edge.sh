ac86304bf734bc6d37ed8d0417d151854bc2cf9e
Tommy <contact@tommytran.io>
2024-05-20 08:39:11 -0700

    Typo fix
    Signed-off-by: Tommy <contact@tommytran.io>

--- fedora-gnome/microsoft-edge.sh
+++ fedora-gnome/microsoft-edge.sh
@@ -29,7 +29,7 @@ sudo dnf install -y microsoft-edge-stable
 
 sudo mkdir -p /etc/opt/edge/policies/managed/ /etc/opt/edge/policies/recommended/
 curl --proxy http://127.0.0.1:8082 https://raw.githubusercontent.com/TommyTran732/Microsoft-Edge-Policies/main/Linux/managed.json | sudo tee /etc/opt/edge/policies/managed/managed.json
-curl --proxy http://127.0.0.1:8082 https://raw.githubusercontent.com/TommyTran732/Microsoft-Edge-Policies/main/Linux/recommended.json | sudo tee /etc/opt/edge/policies/managed/recommended.json
+curl --proxy http://127.0.0.1:8082 https://raw.githubusercontent.com/TommyTran732/Microsoft-Edge-Policies/main/Linux/recommended.json | sudo tee /etc/opt/edge/policies/recommended/recommended.json
 
 # Work around for Edge audio bug
 sudo dnf install -y pulseaudio-utils
