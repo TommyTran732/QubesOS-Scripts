5e3cf1f9796d0004064eab83ea82efdc1093878e
Tommy <contact@tommytran.io>
2024-11-12 07:57:33 -0700

    Typo Fixes
    Signed-off-by: Tommy <contact@tommytran.io>

--- debian-gnome/lokinet.sh
+++ debian-gnome/lokinet.sh
@@ -19,12 +19,12 @@ echo 'binds+=( '\'''/etc/loki''\'' )' | sudo tee /etc/qubes-bind-dirs.d/50_user.
 download https://deb.oxen.io/pub.gpg /usr/share/keyrings/oxen.gpg
 echo "deb [signed-by=/usr/share/keyrings/oxen.gpg] https://deb.oxen.io $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/oxen.list
 
-sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+download https://repository.mullvad.net/deb/mullvad-keyring.asc /usr/share/keyrings/mullvad-keyring.asc
 echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
 
 # Install packages
 sudo apt update
-sudo apt install lokinet mullvad-browser resolvconf
+sudo apt install -y lokinet mullvad-browser resolvconf
 
 download https://raw.githubusercontent.com/TommyTran732/QubesOS-Scripts/main/etc/systemd/system/lokinet-dns-fix.service /etc/systemd/system/lokinet-dns-fix.service
 sudo systemctl enable --now lokinet-dns-fix
