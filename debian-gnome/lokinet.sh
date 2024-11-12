3775461e28bfd8e729b75d1455717615000effeb
Tommy <contact@tommytran.io>
2024-11-12 07:47:48 -0700

    Add mullvad browser to lokinet
    Signed-off-by: Tommy <contact@tommytran.io>

--- debian-gnome/lokinet.sh
+++ debian-gnome/lokinet.sh
@@ -15,11 +15,16 @@ umask 022
 sudo mkdir -p /etc/qubes-bind-dirs.d
 echo 'binds+=( '\'''/etc/loki''\'' )' | sudo tee /etc/qubes-bind-dirs.d/50_user.conf 
 
+# Add repositories
 download https://deb.oxen.io/pub.gpg /usr/share/keyrings/oxen.gpg
 echo "deb [signed-by=/usr/share/keyrings/oxen.gpg] https://deb.oxen.io $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/oxen.list
 
+sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+
+# Install packages
 sudo apt update
-sudo apt install lokinet resolvconf
+sudo apt install lokinet mullvad-browser resolvconf
 
 download https://raw.githubusercontent.com/TommyTran732/QubesOS-Scripts/main/etc/systemd/system/lokinet-dns-fix.service /etc/systemd/system/lokinet-dns-fix.service
 sudo systemctl enable --now lokinet-dns-fix
