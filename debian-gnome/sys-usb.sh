c2e64a605aa8f620829997446517e76ce252983f
Tommy <contact@tommytran.io>
2024-12-30 04:36:52 -0700

    Add Debian based sys-usb

---
+++ debian-gnome/sys-usb.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Copyright (C) 2022-2024 Thien Tran
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+set -eu -o pipefail
+
+unpriv(){
+  sudo -u nobody "${@}"
+}
+
+download() {
+  unpriv curl -s --proxy http://127.0.0.1:8082 "${1}" | sudo tee "${2}" > /dev/null
+}
+
+curl -s --proxy http://127.0.0.1:8082 -L https://github.com/trustcrypto/OnlyKey-App/releases/download/v5.5.0/OnlyKey_5.5.0_amd64.deb -O
+
+sudo apt install gnome-disk-utility ./OnlyKey_5.5.0_amd64.deb
+
+sudo systemctl disable --now hide-hardware-info.service
